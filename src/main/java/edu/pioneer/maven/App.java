c5d9ab5612f1b1fad301139a22b5ceab9d5530ed
rakesh reddy <rakeshreddy.nimmala@gmail.com>
2017-10-23 11:30:32 -0500

    first commit

---
+++ src/main/java/edu/pioneer/maven/App.java
@@ -0,0 +1,13 @@
+package edu.pioneer.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
